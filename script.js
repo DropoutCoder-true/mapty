1e631af18391c412a3804fc0a8a2e9d84a5cbacb
vighnesh <vighneshzagade4@gmail.com>
2024-08-05 23:44:40 +0530

    refactored-the-entire-codebase-using-OOPS

--- script.js
+++ script.js
@@ -11,79 +11,79 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let mapEvent, map; 
+class App {
+  #map;
+  #mapEvent;
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Couldn't get your location");
+        }
+      );
+  }
+
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-
-    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    map = L.map('map').setView([latitude, longitude], 13);
-    // console.log(map); 
+    this.#map = L.map('map').setView([latitude, longitude], 13);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-
-    // L.marker([latitude, longitude])
-    //   .addTo(map)
-    //   .bindPopup('You are here!')
-    //   .openPopup();
+    }).addTo(this.#map);
 
     // Handling click on Map
-    map.on("click", function(mapE){
-      mapEvent = mapE; 
-      form.classList.remove("hidden"); 
-      inputDistance.focus(); 
-
-
-      // console.log(mapEvent); 
-      // const {lat, lng} = mapEvent.latlng; 
-
-      // L.marker([lat, lng])
-      // .addTo(map)
-      // .bindPopup(L.popup({
-      //   maxWidth: 250, 
-      //   minWidth: 100, 
-      //   autoClose: false, 
-      //   closeOnClick: false, 
-      //   className: "running-popup",
-      // }))
-      // .setPopupContent("Workout!") 
-      // .openPopup();
-    })
-  },
-  function () {
-    alert("Couldn't get your location");
+    this.#map.on('click', this._showForm.bind(this));
   }
-);
 
-form.addEventListener("click", function(e){
-  e.preventDefault(); 
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-  // Clearing input fields 
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  // Display marker 
-  console.log(mapEvent);
-  const {lat, lng} = mapEvent.latlng; 
+  _newWorkout(e) {
+    e.preventDefault();
 
-  L.marker([lat, lng])
-  .addTo(map)
-  .bindPopup(L.popup({
-    maxWidth: 250, 
-    minWidth: 100, 
-    autoClose: false, 
-    closeOnClick: false, 
-    className: "running-popup",
-  }))
-  .setPopupContent("Workout!") 
-  .openPopup();
-})
+    // Clearing input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
 
-inputType.addEventListener("change", function(){
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-})
+    // Display marker
+    console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout!')
+      .openPopup();
+  }
+}
+
+const app = new App();
